75279bddfc41d9f5642f4de4c27c64d1b22f7917
Veyyr <dimapolska967@gmail.com>
2026-09-21 15:18:52 +0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const element = document.getElementById('my-element');
+const counter = document.getElementById('counter_info');
+
+let number = 0;
+
+function next() {
+    number += 1
+    counter.innerText = `${number}`
+}
+
+function reset() {
+    number = 0
+    counter.innerText = `${number}`
+}
+
+function back() {
+    number -= 1
+    counter.innerText = `${number}`
+}
