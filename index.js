4aa20ffc99a1588345973501f6d0f56701081da6
Veyyr <dimapolska967@gmail.com>
2026-09-21 15:33:59 +0500

    add comments

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+// поле где увеличиваем или уменьшаем число
 let counter = document.getElementById('counter_info');
-
+// число-счетчик
 let number = 0;
 
+// функция для увеличения числа
 function next() {
-    number += 1
+    number += 1 // увеличить число
     counter.innerText = `${number}`
-    check()
+    check() // проверить цвет
 }
 
+// функция для обнуления счетчика
 function reset() {
-    number = 0
+    number = 0 // обнулить число
     counter.innerText = `${number}`
-    check()
+    check() // проверить цвет
 }
 
+// функция для уменьшения счетчика
 function back() {
-    number -= 1
+    number -= 1 // уменьшить число
     counter.innerText = `${number}` 
-    check()
+    check() // проверить цвет
 }
 
+// проверить число и выдать ему цвет
 function check(){
     if (number == 0) {
-        counter.style.color = "gray"
+        counter.style.color = "gray" // серый есль 0
     } else if (number > 0) {
-        counter.style.color = "green"
+        counter.style.color = "green" // зеленый если положительный
     } else if (number < 0) {
-        counter.style.color = "red"
+        counter.style.color = "red" // красный если меньше 0
     }
 }
